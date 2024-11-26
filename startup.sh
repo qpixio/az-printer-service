4d03cd5dfa116fd6aa0812ef28ad46201f4cd7eb
hatemmaamoun <hatemmaamoun@gmail.com>
2024-11-26 12:37:01 +0200

    npm install in boot

--- startup.sh
+++ startup.sh
@@ -6,5 +6,10 @@ sleep 10
 # Navigate to your print server directory
 cd ~/print-server
 
+# Install dependencies
+echo "Installing dependencies..."
+npm install
+
 # Start PM2
+echo "Starting PM2..."
 pm2 start ecosystem.config.js
