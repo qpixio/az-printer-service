e3fe03c72d0e54c110b708ed8648d4812e2b030d
hatemmaamoun <hatemmaamoun@gmail.com>
2024-11-27 17:40:55 +0200

    dynamic port

--- print-server.js
+++ print-server.js
@@ -8,12 +8,12 @@ app.use(express.json());
 
 app.post('/print', async (req, res) => {
   try {
-    const { content, printerIP = '192.168.123.100' } = req.body;
+    const { content, printerIP = '192.168.123.100', port = 9100 } = req.body;
     
     // Create a connection to the printer
     const client = new net.Socket();
     
-    client.connect(9100, printerIP, () => {
+    client.connect(port, printerIP, () => {
       console.log(`Connected to printer at ${printerIP}`);
       
       // Basic ESC/POS commands
